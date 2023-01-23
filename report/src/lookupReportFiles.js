52498088ff5285b212f69f6c4c21c89d67b0a5f0
Björn Pipiorke <bjoern.pipiorke@prodyna.com>
2023-01-24 00:43:01 +0100

    adapt reporting

--- report/src/lookupReportFiles.js
+++ report/src/lookupReportFiles.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 
 
-glob('./dist/results/**/*.json', (err, files) => {
+glob('./dist/reports/**/*.json', (err, files) => {
     if (err) {
         return console.error(err)
     }
--- report/src/metrics.js
+++ report/src/metrics.js
@@ -1,8 +1,8 @@
 import Chart from 'chart.js/auto';
 import reportFiles from './loadtest-results.json';
 
-const loadTestData = [];
-const imageSizeData = [];
+const loadTestData = {};
+const imageSizeData = {};
 const cpuDataSet = {};
 const memDataSet = {};
 
@@ -19,10 +19,24 @@ function formatBytes(bytes, decimals = 2) {
 
 function prepareContainerImageSizesData(prefix, containerImageData){
     const imageSize = containerImageData["image-size"];
-    console.log("containerImageSize " + prefix, imageSize);
-    imageSizeData.push({
-        "name": prefix,
-        "value": formatBytes(imageSize)
+    // console.log("containerImageSize " + prefix, imageSize);
+
+    imageSizeData
+
+    if(!imageSizeData.labels){
+        imageSizeData.labels=[];
+    }
+
+    if(!imageSizeData.datasets){
+        imageSizeData.datasets=[];
+    }
+    imageSizeData.labels.push(prefix);
+    imageSizeData.datasets.push({
+        "label": prefix,
+        "data": [{
+            "y": formatBytes(imageSize),
+            "x": prefix
+        }]
     });
 }
 
@@ -36,8 +50,23 @@ function prepareLoadTestData(prefix, loadTestResults){
     const httpRequestDurationP90 = loadTestResults.metrics["http_req_duration"].values["p(90)"];
     const httpRequestDurationP95 = loadTestResults.metrics["http_req_duration"].values["p(95)"];
 
-    loadTestData.push({
-        "name": prefix,
+
+    if(!loadTestData.labels){
+        loadTestData.labels=[];
+    }
+
+    if(!loadTestData.datasets){
+        loadTestData.datasets=[];
+    }
+    loadTestData.labels.push(prefix);
+
+    console.log("loadtestData-"+prefix, loadTestResults);
+
+    loadTestData.datasets.push({
+        "label": prefix,
+        data: [{
+            "label": prefix,
+
         "value-avg": httpRequestDurationAvg,
         "name-max": prefix + "-avg",
         "value-max": httpRequestDurationMax,
@@ -50,14 +79,16 @@ function prepareLoadTestData(prefix, loadTestResults){
         "value-p90": httpRequestDurationP90,
         "name-p95": prefix + "-p95",
         "value-p95": httpRequestDurationP95,
+        }]
     });
+    console.log("final-LT-data", loadTestData);
 }
 
 function unifyValues(values){
-    console.log("values", values);
+    // console.log("values", values);
     const result = [];
     var min = Math.min(...values.map(item => item[0]));
-    console.log("min:", min);
+    // console.log("min:", min);
     values.map(item => {
         result.push({
             x: item[0] - min,
@@ -70,55 +101,62 @@ function unifyValues(values){
 }
 
 function prepareMemData(prefix, perfData){
-    console.log("mem-perfData", perfData)
+    // console.log("mem-perfData", perfData)
     const values = perfData.data.result[0].values;
     let unifiedVal = unifyValues(values);
-    console.log("unified values", unifiedVal);
+    // console.log("unified values", unifiedVal);
 
     if(!memDataSet.labels){
         memDataSet.labels=[];
     }
 
+    if(!memDataSet.datasets){
+        memDataSet.datasets=[];
+    }
+
     memDataSet.labels.push(prefix);
-    memDataSet.datasets = [
+    memDataSet.datasets.push(
         {
             label: prefix,
             data: unifiedVal,
         },
-    ];
+    );
 
 }
 
 function prepareCpuData(prefix, perfData){
-    console.log("perfData", perfData)
+    // console.log("cpuData-"+prefix , perfData)
     const values = perfData.data.result[0].values;
     let unifiedVal = unifyValues(values);
-    console.log("unified values", unifiedVal);
+    // console.log("unified values", unifiedVal);
 
     if(!cpuDataSet.labels){
         cpuDataSet.labels=[];
     }
 
+    if(!cpuDataSet.datasets){
+        cpuDataSet.datasets=[];
+    }
+
     cpuDataSet.labels.push(prefix);
-    cpuDataSet.datasets = [
+    cpuDataSet.datasets.push(
         {
             label: prefix,
             data: unifiedVal,
-        },
-    ];
+        });
 }
 
 async function loadServiceData(prefix, serviceReports) {
-
+    console.log("data preparation:", prefix)
     const loadTestResultsPath = serviceReports["loadtest-results"];
     const containerImageSizePath = serviceReports["container-image-size"];
     const perfCPUPath = serviceReports["perf-cpu"];
     const perfMemPath = serviceReports["perf-mem"];
 
-    console.log(loadTestResultsPath, "loadTestResultsPath");
-    console.log(containerImageSizePath, "containerImageSizePath");
-    console.log(perfCPUPath, "perfCPUPath");
-    console.log(perfMemPath, "perfMemPath");
+    // console.log(loadTestResultsPath, "loadTestResultsPath");
+    // console.log(containerImageSizePath, "containerImageSizePath");
+    // console.log(perfCPUPath, "perfCPUPath");
+    // console.log(perfMemPath, "perfMemPath");
 
     const responsesJSON = await Promise.all([
         fetch(loadTestResultsPath),
@@ -127,10 +165,10 @@ async function loadServiceData(prefix, serviceReports) {
         fetch(perfMemPath)
     ]);
     const [loadTestResults, containerImageSize, perfCPU, perfMem] = await Promise.all(responsesJSON.map(r => r.json()));
-    console.log("loadtest", loadTestResults);
-    console.log("containerImages", containerImageSize);
-    console.log('cpu', perfCPU);
-    console.log('mem', perfMem);
+    // console.log("loadtest", loadTestResults);
+    // console.log("containerImages", containerImageSize);
+    // console.log('cpu', perfCPU);
+    // console.log('mem', perfMem);
 
 
     prepareLoadTestData(prefix, loadTestResults);
@@ -143,14 +181,7 @@ async function loadServiceData(prefix, serviceReports) {
 }
 
 
-
-
-
-
-(async function() {
-
-
-
+async function prepareChartData(){
     for (const key in reportFiles) {
 
         console.log(key, reportFiles[key]);
@@ -159,15 +190,23 @@ async function loadServiceData(prefix, serviceReports) {
         await loadServiceData(key, serviceReports)
 
     }
+}
 
 
+
+(async function() {
+    console.log("reportFiles", reportFiles);
+
+    await prepareChartData();
+
+    console.log("start drawing charts");
     // container sizes
+    console.log("image sizes", imageSizeData)
     new Chart(
         document.getElementById('container_image_size'),
         {
             type: 'bar',
             options: {
-
                 plugins: {
                     legend: true,
                     title: {
@@ -177,11 +216,12 @@ async function loadServiceData(prefix, serviceReports) {
                 },
                 scales: {
                     x: {
+                        display: true,
                         type: 'category',
-                        // title: {
-                        //     display: true,
-                        //     text: 'Services',
-                        // },
+                        title: {
+                            display: true,
+                            text: 'Services',
+                        },
                     },
                     y: {
                         display: true,
@@ -193,30 +233,22 @@ async function loadServiceData(prefix, serviceReports) {
                     }
                 },
             },
-            data: {
-                labels: imageSizeData.map(row => row.name),
-                datasets: [
-                    {
-                        label: imageSizeData.map(row => row["name"]),
-                        data: imageSizeData.map(row => row["value"])
-                    },
-                    // {
-                    //     label: 'Http Request Duration Max',
-                    //     data: loadTestData.map(row => row["value-max"])
-                    // },
-                ]
-            }
+            data: imageSizeData,
         }
     );
 
 
     // loadtest results
+    console.log("loadTestData", loadTestData)
     new Chart(
         document.getElementById('http_req_duration_avg'),
         {
             type: 'bar',
             options: {
-
+                parsing: {
+                    xAxisKey: 'label',
+                    yAxisKey: 'value-avg'
+                },
               plugins: {
                   legend: true,
                   title: {
@@ -227,23 +259,37 @@ async function loadServiceData(prefix, serviceReports) {
               scales: {
                   x: {
                       type: 'category',
-                      // labels: ['java-pure']
+                      labels: loadTestData.labels,
+                      title: {
+                          display: true,
+                          text: 'Services',
+                      },
+                  },
+                  y: {
+                      display: true,
+                      type: 'linear',
+                      title: {
+                          display: true,
+                          text: 'Count',
+                      },
                   }
               },
             },
-            data: {
-                labels: loadTestData.map(row => row.name),
-                datasets: [
-                    {
-                        label: loadTestData.map(row => row["name"]),
-                        data: loadTestData.map(row => row["value-avg"])
-                    },
-                    // {
-                    //     label: 'Http Request Duration Max',
-                    //     data: loadTestData.map(row => row["value-max"])
-                    // },
-                ]
-            }
+            data: loadTestData,
+
+            //     {
+            //     labels: loadTestData.map(row => row.name),
+            //     datasets: [
+            //         {
+            //             label: loadTestData.map(row => row["name"]),
+            //             data: loadTestData.map(row => row["value-avg"])
+            //         },
+            //         // {
+            //         //     label: 'Http Request Duration Max',
+            //         //     data: loadTestData.map(row => row["value-max"])
+            //         // },
+            //     ]
+            // }
         }
     );
 

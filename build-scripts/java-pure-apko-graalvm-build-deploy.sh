5addce4488023e67cf35605039f7d6feb3b2007d
Björn Pipiorke <bjoern.pipiorke@prodyna.com>
2023-02-18 17:11:54 +0100

    rename folder graalvm build

--- build-scripts/java-pure-apko-graalvm-build-deploy.sh
+++ build-scripts/java-pure-apko-graalvm-build-deploy.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/"
 SERVICE_FOLDER_NAME="java-pure-apko-graalvm"
 SERVICE_FOLDER="${SCRIPT_DIR}/../${SERVICE_FOLDER_NAME}"
 SERVICE_DOCKER_BUILD_CONTEXT="${SERVICE_FOLDER}/"
-SERVICE_DOCKER_FILE="${SERVICE_FOLDER}/docker/openjdk/DockerfileUsingWolfiBase"
+SERVICE_DOCKER_FILE="${SERVICE_FOLDER}/docker/graalvm/DockerfileUsingWolfiBase"
 
 # building apko / wolfi base image
 docker run --rm -v ${SERVICE_FOLDER}/docker/java-base-apko:/work -w /work cgr.dev/chainguard/apko build --debug "/work/image.yaml" "my-java-base:latest" "my-java-base.tar"
